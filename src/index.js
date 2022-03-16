681ded2f3ea09a11334509c176dbee4f6c41f41a
andrew kim <mr.kimpa4i@gmail.com>
2022-03-16 17:27:21 +0300

    comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-
+// Приветствие
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
 console.log(`Hi ${userName}`);
@@ -8,15 +8,17 @@ console.log(questionOfGame);
 
 const arr = [];
 let count = 0;
-
+// Цикл
 for (let i = 0; i < 3; i += 1) {
   const questionText = '';
   const question = readlineSync.question(questionText);
   console.log(`Your answer:${question}`);
 
+  // Логика игры
   const gameLogic = () => {
   };
 
+  // Проверка
   if (gameLogic() === question) {
     console.log('Correct!');
     count += 1;
