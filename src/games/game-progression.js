80936b563e904c6d2a988d11bd3fc1e3d0a0c1f7
andrew kim <mr.kimpa4i@gmail.com>
2022-04-02 00:16:23 +0300

    fix

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -4,10 +4,10 @@ import getProgression from '../progression.js';
 // Приветствие
 const ruleGame = 'What number is missing in the progression?';
 
-const calculateAnswer = (value, step, length, numOfHiddenNumber) => {
+const calculateAnswer = (value, step, length, numHiddenNumber) => {
   const str = getProgression(value, step, length);
-  const hiddenNumber = str[numOfHiddenNumber];
-  str[numOfHiddenNumber] = '..';
+  const hiddenNumber = str[numHiddenNumber];
+  str[numHiddenNumber] = '..';
   const arr = str.join(' ');
   return [arr, hiddenNumber];
 };
@@ -16,8 +16,8 @@ const getGameData = () => {
   const value = getRandomRange(1, 100);
   const step = getRandomRange(1, 10);
   const length = 10;
-  const numOfHiddenNumber = getRandomRange(0, 9);
-  const [question, correctAnswer] = calculateAnswer(value, step, length, numOfHiddenNumber);
+  const numHiddenNumber = getRandomRange(0, 9);
+  const [question, correctAnswer] = calculateAnswer(value, step, length, numHiddenNumber);
   return [question, String(correctAnswer)];
 };
 
